68d6e763a86e440297461416203783a6e032f8d5
MichalPC <zfac211@rhul.ac.uk>
2020-11-05 06:11:43 +0000

    Create a Home screen component files.

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <main className="Home-container">
+        Login Screen
+      </main>
+    </div>
+  );
+}
+
+export default Home;
