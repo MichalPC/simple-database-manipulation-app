7c0a09ab186cb49f2104bf07f91c1dcc2b58a95a
MichalPC <zfac211@rhul.ac.uk>
2020-11-05 06:09:54 +0000

    Create a Login screen component files.

---
+++ src/components/Login/Login.js
@@ -0,0 +1,13 @@
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="Login">
+      <main className="Login-container">
+        Login Screen
+      </main>
+    </div>
+  );
+}
+
+export default Login;
