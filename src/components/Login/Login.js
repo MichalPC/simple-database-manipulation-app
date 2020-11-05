bdcf5cfd5ff932e831aef93d0980e9a40bfc55ab
MichalPC <zfac211@rhul.ac.uk>
2020-11-05 07:18:29 +0000

    Set value of password using input.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -19,7 +19,7 @@ function Login() {
       <main className="Login-container">
         <form className="Login-form">
           <input type="text" value={curUsername} onChange={setUsername}></input>
-          <input className="Password-input"type="text"></input>
+          <input className="Password-input"type="text" value={curPassword} onChange={setPassword}></input>
           <button type='submit'>Login</button>
         </form>
         <Link className='Login-button' to={'/home'} className="nav-link"> Login </Link>
