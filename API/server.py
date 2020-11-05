d9e6e8a8eecd4100889d04b58d083ecbe8d3bb64
MichalPC <zfac211@rhul.ac.uk>
2020-11-05 16:09:13 +0000

    Connect to a remote mongodb database.

--- API/server.py
+++ API/server.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 import pprint
 
+#VERY UNSAFE#
+client = MongoClient("mongodb+srv://michalpc:test1234@simple-user-login-app-d.ki5ky.mongodb.net/db?retryWrites=true&w=majority")
+db = client['db']
+collection = db['users']
+
 app = FastAPI()
 
 @app.get("/")
