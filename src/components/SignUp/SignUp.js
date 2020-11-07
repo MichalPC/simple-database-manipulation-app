93bfdca5e77a4a491789eaa70aa4eb5ae5103779
MichalPC <zfac211@rhul.ac.uk>
2020-11-07 21:03:04 +0000

    Create files for SignUp component.

---
+++ src/components/SignUp/SignUp.js
@@ -0,0 +1,21 @@
+import './SignUp.css';
+import { Link, useHistory } from 'react-router-dom'
+import React, { useState } from 'react';
+
+function SignUp() {
+    const [curUsername, setCurUsername] = useState('')
+    const [curPassword, setCurPassword] = useState('')
+    const history = useHistory();
+
+    return (
+        <div className="SignUp">
+            <main className="SignUp-container">
+                <form className="SignUp-form">
+                    <input type="text" className="User-input" value={curUsername} onChange={setUsername}></input>
+                    <input type="text" className="Password-input"  value={curPassword} onChange={setPassword}></input>
+                    <button type='button' onClick={}>Sign Up</button>
+                </form>
+            </main>
+        </div>
+    );
+}
